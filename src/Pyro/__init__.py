cf9f36eeb32e2c6b9c4f6c6baecee525bd4f35d8
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2009-12-27 17:26:53 +0000

    version check

--- src/Pyro/__init__.py
+++ src/Pyro/__init__.py
@@ -1,6 +1,11 @@
 # Pyro package.
 # Contains some generic init stuff.
 
+import sys
+if sys.version_info<(2,5):
+    raise RuntimeError("Pyro requires at least Python 2.5")
+
+
 import logging
 
 if len(logging.root.handlers)==0:
