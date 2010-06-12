c78b53b8a74d99cc64de9eb6d5de4170fa6ff4a0
Irmen <Irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2010-06-12 21:47:18 +0000

    added setup script

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import sys
+
+if __name__ == '__main__' :
+    sys.path.insert(0, "src")
+    import Pyro.constants
+    print 'Pyro version =',Pyro.constants.VERSION
+
+    setup(name="Pyro",
+        version= Pyro.constants.VERSION,
+        license="MIT",
+        description = "distributed object middleware for Python (RPC)",
+        long_description = """Pyro stands for PYthon Remote Objects. It is an advanced and powerful Distributed Object Technology system written entirely in Python, that is designed to be fast and very easy to use.""",
+        author = "Irmen de Jong",
+        author_email="irmen@razorvine.net",
+        keywords="distributed objects, middleware, network communication, RMI, IPC, DOT",
+        url = "http://www.razorvine.net/python/Pyro",
+        download_url="http://www.xs4all.nl/~irmen/pyro4/download/",
+        package_dir={'':'src'},
+        packages=['Pyro', 'Pyro.socketserver'],
+        scripts = [],
+        platforms="any",
+        classifiers=[
+                "Development Status :: 4 - Beta",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python",
+                "Topic :: Software Development :: Object Brokering",
+                "Topic :: System :: Distributed Computing",
+                "Topic :: System :: Networking"
+            ]
+    )
