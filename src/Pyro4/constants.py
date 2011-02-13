4e2223d7125524112c34dcd06abc8256f26dd0fe
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2011-02-13 16:43:14 +0000

    version string

--- src/Pyro4/constants.py
+++ src/Pyro4/constants.py
@@ -6,7 +6,7 @@ irmen@razorvine.net - http://www.razorvine.net/python/Pyro
 """
 
 # Pyro version
-VERSION = '4.3'
+VERSION = '4.4-svn'
 
 # standard object name for the Daemon object
 DAEMON_NAME = "Pyro.Daemon"
