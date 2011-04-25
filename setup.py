9ef7d57bc249a88036279f24e58161696f03e8a3
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2011-04-25 16:09:18 +0000

    download link removed because I decided to upload the files to Pypi instead.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ if __name__ == '__main__' :
         author_email="irmen@razorvine.net",
         keywords="distributed objects, middleware, network communication, RMI, IPC, DOT",
         url = "http://www.xs4all.nl/~irmen/pyro4/",
-        download_url="http://www.xs4all.nl/~irmen/pyro4/download/",
         package_dir={'':'src'},
         packages=['Pyro4', 'Pyro4.socketserver'],
         scripts = [],
