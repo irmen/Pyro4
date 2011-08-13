3e0047603763d44de9c76a6cb9057766dc1ba45e
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2011-08-13 14:18:33 +0000

    enable setup.py test command when using setuptools/distribute

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 import sys
 try:
-    # try setuptools first, to get access to build_sphinx command
+    # try setuptools first, to get access to build_sphinx and test commands
     from setuptools import setup
+    using_setuptools=True
 except ImportError:
     from distutils.core import setup
+    using_setuptools=False
 
 if __name__ == '__main__' :
     sys.path.insert(0, "src")
     import Pyro4.constants
     print('Pyro version = %s' % Pyro4.constants.VERSION)
 
-    setup(name="Pyro4",
-        version= Pyro4.constants.VERSION,
-        license="MIT",
-        description = "distributed object middleware for Python (RPC)",
-        long_description = """Pyro means PYthon Remote Objects. 
+    setupargs={
+        "name": "Pyro4",
+        "version": Pyro4.constants.VERSION,
+        "license": "MIT",
+        "description": "distributed object middleware for Python (RPC)",
+        "long_description": """Pyro means PYthon Remote Objects. 
 It is a library that enables you to build applications in which
 objects can talk to eachother over the network, with minimal programming effort.
 You can just use normal Python method calls, with almost every possible parameter
@@ -24,15 +27,15 @@ generally stay out of your way. But it also provides a set of powerful features 
 enables you to build distributed applications rapidly and effortlessly.
 Pyro is written in 100% pure Python and therefore runs on many platforms and Python versions,
 including Python 2.x, Python 3.x, IronPython, Jython and Pypy.""",
-        author = "Irmen de Jong",
-        author_email="irmen@razorvine.net",
-        keywords="distributed objects, middleware, network communication, remote method call, IPC",
-        url = "http://irmen.home.xs4all.nl/pyro/",
-        package_dir={'':'src'},
-        packages=['Pyro4', 'Pyro4.socketserver', 'Pyro4.test'],
-        scripts = [],
-        platforms="any",
-        classifiers=[
+        "author": "Irmen de Jong",
+        "author_email": "irmen@razorvine.net",
+        "keywords": "distributed objects, middleware, network communication, remote method call, IPC",
+        "url": "http://irmen.home.xs4all.nl/pyro/",
+        "package_dir": {'':'src'},
+        "packages": ['Pyro4', 'Pyro4.socketserver', 'Pyro4.test'],
+        "scripts": [],
+        "platforms": "any",
+        "classifiers": [
                 "Development Status :: 5 - Production/Stable",
                 "Intended Audience :: Developers",
                 "License :: OSI Approved :: MIT License",
@@ -48,7 +51,12 @@ including Python 2.x, Python 3.x, IronPython, Jython and Pypy.""",
                 "Topic :: System :: Distributed Computing",
                 "Topic :: System :: Networking"
             ]
-    )
+        }
+        
+    if using_setuptools:
+        setupargs["test_suite"]="nose.collector"    # use Nose to run unittests
+    
+    setup(**setupargs)
     
     if len(sys.argv)>=2 and sys.argv[1].startswith("install"):
         print("\nOnly the Pyro library has been installed (version %s)." % Pyro4.constants.VERSION)
