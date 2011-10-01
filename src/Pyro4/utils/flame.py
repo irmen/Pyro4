af9e5fa83d88a3ddca56b3939087d8658ae83b81
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2011-10-01 20:24:54 +0000

    moved Flame to Pyro4.utils package

--- src/Pyro4/utils/flame.py
+++ src/Pyro4/utils/flame.py
@@ -2,11 +2,6 @@
 Pyro FLAME:  Foreign Location Automatic Module Exposer.
 Easy but potentially very dangerous way of exposing remote modules and builtins.
 
-You can start this module as a script from the command line, to easily get a
-flame server running:
-
-  :command:`python -m Pyro4.flame`
-
 Pyro - Python Remote Objects.  Copyright by Irmen de Jong (irmen@razorvine.net).
 """
 
@@ -29,7 +24,7 @@ try:
 except ImportError:
     from io import StringIO
 
-__all__ = ["connect", "Flame"]
+__all__ = ["connect", "start", "Flame"]
 
 
 # Exec is a statement in Py2, a function in Py3
@@ -230,6 +225,14 @@ class Flame(object):
         return method(*args, **kwargs)
 
 
+def start(daemon):
+    """
+    Create and register a Flame server in the given daemon.
+    Be *very* cautious before starting this: it allows the clients full access to everything on your system.
+    """
+    return daemon.register(Flame(), Pyro4.constants.FLAME_NAME)
+
+
 def connect(location):
     """
     Connect to a Flame server on the given location, for instance localhost:9999 or ./u:unixsock
@@ -238,42 +241,3 @@ def connect(location):
     proxy = Pyro4.core.Proxy("PYRO:%s@%s" % (Pyro4.constants.FLAME_NAME, location))
     proxy._pyroBind()
     return proxy
-
-
-def main(args, returnWithoutLooping=False):
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option("-H", "--host", default="localhost", help="hostname to bind server on (default=localhost)")
-    parser.add_option("-p", "--port", type="int", default=0, help="port to bind server on")
-    parser.add_option("-u", "--unixsocket", help="Unix domain socket name to bind server on")
-    parser.add_option("-q", "--quiet", action="store_true", default=False, help="don't output anything")
-    parser.add_option("-k", "--key", help="the HMAC key to use")
-    options, args = parser.parse_args(args)
-
-    if not options.quiet:
-        print("Starting Pyro Flame server.")
-
-    hmac = options.key
-    if not hmac:
-        print("Warning: HMAC key not set. Anyone can connect to this server!")
-    if hmac and sys.version_info >= (3, 0):
-        hmac = bytes(hmac, "utf-8")
-    Pyro4.config.HMAC_KEY = hmac or Pyro4.config.HMAC_KEY
-    if not options.quiet and Pyro4.config.HMAC_KEY:
-        print("HMAC_KEY set to: %s" % Pyro4.config.HMAC_KEY)
-
-    daemon = Pyro4.core.Daemon(host=options.host, port=options.port, unixsocket=options.unixsocket)
-    uri = daemon.startFlame()
-    if not options.quiet:
-        print("server uri: %s" % uri)
-        print("server is running.")
-
-    if returnWithoutLooping:
-        return daemon, uri        # for unit testing
-    else:
-        daemon.requestLoop()
-    daemon.close()
-    return 0
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
