4aaf7e7325dbd9e65abe4819f1d577472515e176
Irmen de Jong <irmen@razorvine.net>
2013-08-17 04:29:20 +0200

    Implemented extensible wire protocol message format using annotation chunks. HMAC is now one of these chunks. HMAC message flag removed.

--- src/Pyro4/message.py
+++ src/Pyro4/message.py
@@ -9,11 +9,12 @@ import hashlib
 import hmac
 import struct
 import logging
+import sys
 
 import Pyro4
 from Pyro4 import constants, errors
 
-__all__ = ["Message", "PingMessage"]
+__all__ = ["Message"]
 
 log = logging.getLogger("Pyro4.message")
 
@@ -26,8 +27,7 @@ MSG_PING = 6
 FLAGS_EXCEPTION = 1<<0
 FLAGS_COMPRESSED = 1<<1
 FLAGS_ONEWAY = 1<<2
-FLAGS_HMAC = 1<<3
-FLAGS_BATCH = 1<<4
+FLAGS_BATCH = 1<<3
 SERIALIZER_SERPENT = 1
 SERIALIZER_JSON = 2
 SERIALIZER_MARSHAL = 3
@@ -37,87 +37,119 @@ SERIALIZER_PICKLE = 4
 class Message(object):
     """
     Pyro write protocol message.
-    Note that it doesn't deal with the actual payload,
-    (De) serialization and handling of the data is done elsewhere.
+
+    Wire messages contains of a fixed size header, an optional set of annotation chunks,
+    and then the payload data. This class doesn't deal with the payload data:
+    (de)serialization and handling of that data is done elsewhere.
+    Annotation chunks are only parsed, except the 'HMAC' chunk: that is created
+    and validated because it is used as a message digest.
+
+    The header format is::
+
+       4   id ('PYRO')
+       2   protocol version
+       2   message type
+       2   message flags
+       2   sequence number
+       4   data length
+       2   data serialization format (serializer id)
+       2   annotations length (total of all chunks, 0 if no annotation chunks present)
+       2   (reserved)
+       2   checksum
+
+    After the header, zero or more annotation chunks may follow, of the format::
+
+       4   id
+       2   chunk length
+       x   annotation chunk databytes
+
+    After that, the actual payload data bytes follow.
+
+    The sequencenumber is used to check if response messages correspond to the
+    actual request message. This prevents the situation where Pyro would perhaps return
+    the response data from another remote call (which would not result in an error otherwise!)
+    This could happen for instance if the socket data stream gets out of sync, perhaps due To
+    some form of signal that interrupts I/O.
+
+    The header checksum is a simple sum of the header fields to make reasonably sure
+    that we are dealing with an actual correct PYRO protocol header and not some random
+    data that happens to start with the 'PYRO' protocol identifier.
+
+    An 'HMAC' annotation chunk contains the hmac digest of the message data bytes and
+    all of the annotation chunk data bytes (except those of the HMAC chunk itself).
     """
-    __slots__ = ["type", "flags", "seq", "data", "datasize", "serializer_id", "hmac"]
-    # The header format is:
-    #   4   id ('PYRO')
-    #   2   version
-    #   2   message type
-    #   2   flags
-    #   2   sequence number
-    #   4   data length
-    #   2   data serialization format (serializer id)
-    #   2   annotations length (currently unused)
-    #   2   (reserved)
-    #   2   checksum
-    #   20  hmac (or all 0)
-    # note: the sequencenumber is used to check if response messages correspond to the
-    # actual request message. This prevents the situation where Pyro would perhaps return
-    # the response data from another remote call (which would not result in an error otherwise!)
-    # This could happen for instance if the socket data stream gets out of sync, perhaps due To
-    # some form of signal that interrupts I/O.
-    # The header checksum is a simple sum of the header fields to make reasonably sure
-    # that we are dealing with an actual correct PYRO protocol header and not some random
-    # data that happens to start with the 'PYRO' protocol identifier.
-    header_format = '!4sHHHHiHHHH20s'
+    __slots__ = ["type", "flags", "seq", "data", "data_size", "serializer_id", "annotations", "annotations_size"]
+    header_format = '!4sHHHHiHHHH'
     header_size = struct.calcsize(header_format)
     checksum_magic = 0x34E9
 
-    def __init__(self, msgType, databytes, serializer_id, flags, seq):
+    def __init__(self, msgType, databytes, serializer_id, flags, seq, annotations=None):
         self.type = msgType
         self.flags = flags
         self.seq = seq
         self.data = databytes
-        self.datasize = len(self.data)
+        self.data_size = len(self.data)
         self.serializer_id = serializer_id
-        if 0 < Pyro4.config.MAX_MESSAGE_SIZE < self.datasize:
-            raise errors.ProtocolError("max message size exceeded (%d where max=%d)" % (self.datasize, Pyro4.config.MAX_MESSAGE_SIZE))
+        self.annotations = annotations or {}
         if Pyro4.config.HMAC_KEY:
-            self.flags |= FLAGS_HMAC
-            self.hmac = hmac.new(Pyro4.config.HMAC_KEY, databytes, digestmod=hashlib.sha1).digest()
-        else:
-            self.hmac = b"\0"*20
+            self.annotations[b"HMAC"] = self.hmac()
+        self.annotations_size = sum([6+len(v) for v in self.annotations.values()])
+        if 0 < Pyro4.config.MAX_MESSAGE_SIZE < (self.data_size + self.annotations_size):
+            raise errors.ProtocolError("max message size exceeded (%d where max=%d)" % (self.data_size+self.annotations_size, Pyro4.config.MAX_MESSAGE_SIZE))
 
     def __repr__(self):
-        return "<%s.%s at %x, type=%d flags=%d seq=%d datasize=%d>" % (self.__module__, self.__class__.__name__, id(self), self.type, self.flags, self.seq, self.datasize)
+        return "<%s.%s at %x, type=%d flags=%d seq=%d datasize=%d #ann=%d>" % (self.__module__, self.__class__.__name__, id(self), self.type, self.flags, self.seq, self.data_size, len(self.annotations))
 
     def to_bytes(self):
-        """creates a byte stream containing the header followed by the data"""
-        return self.__header_bytes() + self.data
+        """creates a byte stream containing the header followed by annotations (if any) followed by the data"""
+        return self.__header_bytes() + self.__annotations_bytes() + self.data
 
     def __header_bytes(self):
-        checksum = (self.type+constants.PROTOCOL_VERSION+self.datasize+self.serializer_id+self.flags+self.seq+self.checksum_magic)&0xffff
-        return struct.pack(self.header_format, b"PYRO", constants.PROTOCOL_VERSION, self.type, self.flags, self.seq, len(self.data), self.serializer_id, 0, 0, checksum, self.hmac)
+        checksum = (self.type+constants.PROTOCOL_VERSION+len(self.data)+self.annotations_size+self.serializer_id+self.flags+self.seq+self.checksum_magic)&0xffff
+        return struct.pack(self.header_format, b"PYRO", constants.PROTOCOL_VERSION, self.type, self.flags, self.seq, self.data_size, self.serializer_id, self.annotations_size, 0, checksum)
 
-    @classmethod
-    def from_header(cls, headerData):
-        """Parses a message header. Does not yet process the message data."""
-        if not headerData or len(headerData)!=cls.header_size:
-            raise errors.ProtocolError("header data size mismatch")
-        tag, ver, msgType, flags, seq, dataLen, serializer_id, annotationsLen, _, headerchecksum, datahmac = struct.unpack(cls.header_format, headerData)
-        if tag!=b"PYRO" or ver!=constants.PROTOCOL_VERSION:
-            raise errors.ProtocolError("invalid data or unsupported protocol version")
-        if headerchecksum!=(msgType+ver+dataLen+flags+serializer_id+seq+cls.checksum_magic)&0xffff:
-            raise errors.ProtocolError("header checksum mismatch")
-        msg = Message(msgType, b"", serializer_id, flags, seq)
-        msg.datasize = dataLen
-        msg.hmac = datahmac
-        return msg
+    def __annotations_bytes(self):
+        a = []
+        for k, v in self.annotations.items():
+            a.append(struct.pack("!4sH", k, len(v)))
+            a.append(v)
+        if sys.platform=="cli":
+            return "".join(a)
+        return b"".join(a)
 
     def send(self, connection):
         """send the message as bytes over the connection"""
         connection.send(self.__header_bytes())
+        if self.annotations:
+            connection.send(self.__annotations_bytes())
         connection.send(self.data)
 
     @classmethod
+    def from_header(cls, headerData):
+        """Parses a message header. Does not yet process the annotations chunks and message data."""
+        if not headerData or len(headerData)!=cls.header_size:
+            raise errors.ProtocolError("header data size mismatch")
+        tag, ver, msg_type, flags, seq, data_size, serializer_id, annotations_size, _, checksum = struct.unpack(cls.header_format, headerData)
+        if tag!=b"PYRO" or ver!=constants.PROTOCOL_VERSION:
+            raise errors.ProtocolError("invalid data or unsupported protocol version")
+        if checksum!=(msg_type+ver+data_size+annotations_size+flags+serializer_id+seq+cls.checksum_magic)&0xffff:
+            raise errors.ProtocolError("header checksum mismatch")
+        msg = Message(msg_type, b"", serializer_id, flags, seq)
+        msg.data_size = data_size
+        msg.annotations_size = annotations_size
+        return msg
+
+    @classmethod
     def recv(cls, connection, requiredMsgTypes=None):
-        """Receives a pyro message from a given connection. Accepts the given message types (None=any, or pass a sequence)."""
-        headerdata = connection.recv(cls.header_size)
-        msg = cls.from_header(headerdata)
-        if 0 < Pyro4.config.MAX_MESSAGE_SIZE < msg.datasize:
-            errorMsg = "max message size exceeded (%d where max=%d)" % (msg.datasize, Pyro4.config.MAX_MESSAGE_SIZE)
+        """
+        Receives a pyro message from a given connection.
+        Accepts the given message types (None=any, or pass a sequence).
+        Also reads annotation chunks and the actual payload data.
+        Validates a HMAC chunk if present.
+        """
+        msg = cls.from_header(connection.recv(cls.header_size))
+        if 0 < Pyro4.config.MAX_MESSAGE_SIZE < (msg.data_size+msg.annotations_size):
+            errorMsg = "max message size exceeded (%d where max=%d)" % (msg.data_size+msg.annotations_size, Pyro4.config.MAX_MESSAGE_SIZE)
             log.error("connection "+str(connection)+": "+errorMsg)
             connection.close()   # close the socket because at this point we can't return the correct sequence number for returning an error message
             raise errors.ProtocolError(errorMsg)
@@ -125,14 +157,31 @@ class Message(object):
             err = "invalid msg type %d received" % msg.type
             log.error(err)
             raise errors.ProtocolError(err)
-        msg.data = connection.recv(msg.datasize)
-        local_hmac_set = Pyro4.config.HMAC_KEY is not None and len(Pyro4.config.HMAC_KEY) > 0
-        if msg.flags&FLAGS_HMAC and local_hmac_set:
-            if msg.hmac != hmac.new(Pyro4.config.HMAC_KEY, msg.data, digestmod=hashlib.sha1).digest():
+        if msg.annotations_size:
+            # read annotation chunks
+            annotations_data = connection.recv(msg.annotations_size)
+            msg.annotations = {}
+            i = 0
+            while i < msg.annotations_size:
+                anno, length = struct.unpack("!4sH", annotations_data[i:i+6])
+                msg.annotations[anno] = annotations_data[i+6:i+6+length]
+                i += 6+length
+        # read data
+        msg.data = connection.recv(msg.data_size)
+        if b"HMAC" in msg.annotations and Pyro4.config.HMAC_KEY:
+            if msg.annotations[b"HMAC"] != msg.hmac():
                 raise errors.SecurityError("message hmac mismatch")
-        elif msg.flags&FLAGS_HMAC != local_hmac_set:
+        elif (b"HMAC" in msg.annotations) != bool(Pyro4.config.HMAC_KEY):
             # Message contains hmac and local HMAC_KEY not set, or vice versa. This is not allowed.
             err = "hmac key config not symmetric"
             log.warning(err)
             raise errors.SecurityError(err)
         return msg
+
+    def hmac(self):
+        """returns the hmac of the data and the annotation chunk values (except HMAC chunk itself)"""
+        mac = hmac.new(Pyro4.config.HMAC_KEY, self.data, digestmod=hashlib.sha1)
+        for k, v in self.annotations.items():
+            if k != b"HMAC":
+                mac.update(v)
+        return mac.digest()
--- tests/PyroTests/test_message.py
+++ tests/PyroTests/test_message.py
@@ -11,9 +11,29 @@ import Pyro4.message
 from Pyro4.message import Message
 import Pyro4.constants
 import Pyro4.util
+import Pyro4.errors
 
 
-class MessageTests(unittest.TestCase):
+def pyrohmac(data, annotations={}):
+    mac = hmac.new(Pyro4.config.HMAC_KEY, data, digestmod=hashlib.sha1)
+    for k, v in annotations.items():
+        if k != b'HMAC':
+            mac.update(v)
+    return mac.digest()
+
+
+class ConnectionMock(object):
+    def __init__(self, data=b""):
+        self.received = data
+    def send(self, data):
+        self.received += data
+    def recv(self, datasize):
+        chunk = self.received[:datasize]
+        self.received = self.received[datasize:]
+        return chunk
+
+
+class MessageTestsHmac(unittest.TestCase):
     def setUp(self):
         Pyro4.config.HMAC_KEY = b"testsuite"
         self.ser = Pyro4.util.get_serializer(Pyro4.config.SERIALIZER)
@@ -21,42 +41,42 @@ class MessageTests(unittest.TestCase):
     def tearDown(self):
         Pyro4.config.HMAC_KEY = None
 
-    def testMsgFactory(self):
-        def pyrohmac(data):
-            return hmac.new(Pyro4.config.HMAC_KEY, data, digestmod=hashlib.sha1).digest()
+    def testMessage(self):
         Message(99, b"", self.ser.serializer_id, 0, 0) # doesn't check msg type here
         self.assertRaises(Pyro4.errors.ProtocolError, Message.from_header, "FOOBAR")
         msg = Message(Pyro4.message.MSG_CONNECT, b"hello", self.ser.serializer_id, 0, 0)
         self.assertEqual(Pyro4.message.MSG_CONNECT, msg.type)
-        self.assertEqual(5, msg.datasize)
+        self.assertEqual(5, msg.data_size)
         self.assertEqual(b"hello", msg.data)
-        self.assertEqual(Pyro4.message.FLAGS_HMAC, msg.flags)
+        self.assertEqual(4+2+20, msg.annotations_size)
+        mac = pyrohmac(b"hello", msg.annotations)
+        self.assertDictEqual({b"HMAC": mac}, msg.annotations)
 
-        hdr = msg.to_bytes()[:-5]
+        hdr = msg.to_bytes()[:24]
         msg = Message.from_header(hdr)
         self.assertEqual(Pyro4.message.MSG_CONNECT, msg.type)
-        self.assertEqual(Pyro4.message.FLAGS_HMAC, msg.flags)
-        self.assertEqual(5, msg.datasize)
-        self.assertEqual(pyrohmac(b"hello"), msg.hmac)
+        self.assertEqual(4+2+20, msg.annotations_size)
+        self.assertEqual(5, msg.data_size)
 
-        hdr = Message(Pyro4.message.MSG_RESULT, b"", self.ser.serializer_id, 0, 0).to_bytes()
+        hdr = Message(Pyro4.message.MSG_RESULT, b"", self.ser.serializer_id, 0, 0).to_bytes()[:24]
         msg = Message.from_header(hdr)
         self.assertEqual(Pyro4.message.MSG_RESULT, msg.type)
-        self.assertEqual(Pyro4.message.FLAGS_HMAC, msg.flags)
-        self.assertEqual(0, msg.datasize)
+        self.assertEqual(4+2+20, msg.annotations_size)
+        self.assertEqual(0, msg.data_size)
 
-        hdr = Message(Pyro4.message.MSG_RESULT, b"hello", self.ser.serializer_id, 42, 0).to_bytes()[:-5]
+        hdr = Message(Pyro4.message.MSG_RESULT, b"hello", 99, 42, 0).to_bytes()[:24]
         msg = Message.from_header(hdr)
         self.assertEqual(Pyro4.message.MSG_RESULT, msg.type)
         self.assertEqual(42, msg.flags)
-        self.assertEqual(5, msg.datasize)
+        self.assertEqual(5, msg.data_size)
+        self.assertEqual(99, msg.serializer_id)
 
         msg = Message(255, b"", self.ser.serializer_id, 0, 255).to_bytes()
-        self.assertEqual(44, len(msg))
+        self.assertEqual(50, len(msg))
         msg = Message(1, b"", self.ser.serializer_id, 0, 255).to_bytes()
-        self.assertEqual(44, len(msg))
+        self.assertEqual(50, len(msg))
         msg = Message(1, b"", self.ser.serializer_id, flags=253, seq=254).to_bytes()
-        self.assertEqual(44, len(msg))
+        self.assertEqual(50, len(msg))
 
         # compression is a job of the code supplying the data, so the messagefactory should leave it untouched
         data = b"x"*1000
@@ -64,12 +84,98 @@ class MessageTests(unittest.TestCase):
         msg2 = Message(Pyro4.message.MSG_INVOKE, data, self.ser.serializer_id, Pyro4.message.FLAGS_COMPRESSED, 0).to_bytes()
         self.assertEqual(len(msg), len(msg2))
 
-    def testMsgFactoryProtocolVersion(self):
+    def testAnnotations(self):
+        annotations = { b"TEST": b"abcde" }
+        msg = Message(Pyro4.message.MSG_CONNECT, b"hello", self.ser.serializer_id, 0, 0, annotations)
+        data = msg.to_bytes()
+        annotations_size = 4+2+20 + 4+2+5
+        self.assertEqual(msg.header_size + 5 + annotations_size, len(data))
+        self.assertEqual(annotations_size, msg.annotations_size)
+        self.assertEqual(2, len(msg.annotations))
+        self.assertEqual(b"abcde", msg.annotations[b"TEST"])
+        mac = pyrohmac(b"hello", annotations)
+        self.assertEqual(mac, msg.annotations[b"HMAC"])
+
+    def testRecvAnnotations(self):
+        annotations = { b"TEST": b"abcde" }
+        msg = Message(Pyro4.message.MSG_CONNECT, b"hello", self.ser.serializer_id, 0, 0, annotations)
+        c = ConnectionMock()
+        msg.send(c)
+        msg = Message.recv(c)
+        self.assertEquals(0, len(c.received))
+        self.assertEquals(5, msg.data_size)
+        self.assertEquals(b"hello", msg.data)
+        self.assertEquals(b"abcde", msg.annotations[b"TEST"])
+        self.assertTrue(b"HMAC" in msg.annotations)
+
+    def testProtocolVersion(self):
         version = Pyro4.constants.PROTOCOL_VERSION
         Pyro4.constants.PROTOCOL_VERSION = 0     # fake invalid protocol version number
         msg = Message(Pyro4.message.MSG_RESULT, b"", self.ser.serializer_id, 0, 1).to_bytes()
         Pyro4.constants.PROTOCOL_VERSION = version
         self.assertRaises(Pyro4.errors.ProtocolError, Message.from_header, msg)
+
+    def testHmac(self):
+        try:
+            hk = Pyro4.config.HMAC_KEY
+            Pyro4.config.HMAC_KEY = b"test key"
+            data = Message(Pyro4.message.MSG_RESULT, b"test", 42, 0, 1).to_bytes()
+            c = ConnectionMock(data)
+        finally:
+            Pyro4.config.HMAC_KEY = hk
+        # test checking of different hmacs
+        try:
+            Message.recv(c)
+            self.fail("crash expected")
+        except Pyro4.errors.SecurityError as x:
+            self.assertTrue("hmac" in str(x))
+        c = ConnectionMock(data)
+        # test that it works again when resetting the key
+        try:
+            hk = Pyro4.config.HMAC_KEY
+            Pyro4.config.HMAC_KEY = b"test key"
+            Message.recv(c)
+        finally:
+            Pyro4.config.HMAC_KEY = hk
+        c = ConnectionMock(data)
+        # test that it doesn't work when no key is set
+        try:
+            hk = Pyro4.config.HMAC_KEY
+            Pyro4.config.HMAC_KEY = b""
+            Message.recv(c)
+            self.fail("crash expected")
+        except Pyro4.errors.SecurityError as x:
+            self.assertTrue("hmac key config" in str(x))
+        finally:
+            Pyro4.config.HMAC_KEY = hk
+
+
+    def testChecksum(self):
+        msg = Message(Pyro4.message.MSG_RESULT, b"test", 42, 0, 1)
+        c = ConnectionMock()
+        msg.send(c)
+        # corrupt the checksum bytes
+        data = c.received
+        data = data[:msg.header_size-2] + b'\x00\x00' + data[msg.header_size:]
+        c = ConnectionMock(data)
+        try:
+            Message.recv(c)
+            self.fail("crash expected")
+        except Pyro4.errors.ProtocolError as x:
+            self.assertTrue("checksum" in str(x))
+
+
+class MessageTestsNoHmac(unittest.TestCase):
+    def testRecvNoAnnotations(self):
+        msg = Message(Pyro4.message.MSG_CONNECT, b"hello", 42, 0, 0)
+        c = ConnectionMock()
+        msg.send(c)
+        msg = Message.recv(c)
+        self.assertEquals(0, len(c.received))
+        self.assertEquals(5, msg.data_size)
+        self.assertEquals(b"hello", msg.data)
+        self.assertEquals(0, msg.annotations_size)
+        self.assertEquals(0, len(msg.annotations))
 
 
 if __name__ == "__main__":
