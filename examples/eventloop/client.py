7ab14df3a8c053ad0dbc0f278377fc7b3a21f879
Irmen de Jong <irmen@razorvine.net>
2013-08-20 12:08:16 +0200

    loop of calls in eventloop example due to speed problems on ubuntu

--- examples/eventloop/client.py
+++ examples/eventloop/client.py
@@ -10,6 +10,7 @@ with Pyro4.core.Proxy("PYRONAME:example.embedded.server") as proxy:
     print("5*11=%d" % proxy.multiply(5,11))
     print("'x'*10=%s" % proxy.multiply('x',10))
 
-    input("press enter to do two more calls:")
-    print("2*4=%d" % proxy.multiply(2,4))
-    print("'@'*3=%s" % proxy.multiply('@',3))
+    input("press enter to do a loop of some more calls:")
+    for i in range(1,20):
+        print("2*i=%d" % proxy.multiply(2,i))
+        print("'@'*i=%s" % proxy.multiply('@',i))
