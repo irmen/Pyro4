07cf001b674cb7da6056d97f997cc5089fddee35
Irmen de Jong <irmen@razorvine.net>
2015-06-23 23:36:49 +0200

    added distributed-computing2 example

--- examples/distributed-computing2/servers.py
+++ examples/distributed-computing2/servers.py
@@ -27,7 +27,7 @@ class WordCounter(object):
                 interesting_words = [w for w in line.split() if w.isalpha() and w not in self.filter_words]
                 counts.update(interesting_words)
             if num % 10 == 0:
-                time.sleep(0.002)  # artificial delay
+                time.sleep(0.01)  # artificial delay
         return counts
 
 
