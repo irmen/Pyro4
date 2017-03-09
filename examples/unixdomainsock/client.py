63a0bfe363bc797b6a926fe47dc45307daf09395
Irmen de Jong <irmen@razorvine.net>
2017-03-10 00:49:14 +0100

    fix example

--- examples/unixdomainsock/client.py
+++ examples/unixdomainsock/client.py
@@ -8,6 +8,10 @@ if sys.version_info < (3, 0):
     input = raw_input
 
 uri = input("enter the server uri: ").strip()
+if "\\x00" in uri:
+    uri=uri.replace("\\x00", "\x00")
+    print("(uri contains 0-byte)")
+
 with Pyro4.Proxy(uri) as p:
     response = p.message("Hello there!")
     print("Response was:", response)
