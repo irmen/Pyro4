8018abeca613242012390680a475bf3497f35f22
Irmen de Jong <irmen@razorvine.net>
2017-10-30 22:00:46 +0100

    release 4.63

--- src/Pyro4/constants.py
+++ src/Pyro4/constants.py
@@ -5,7 +5,7 @@ Pyro - Python Remote Objects.  Copyright by Irmen de Jong (irmen@razorvine.net).
 """
 
 # Pyro version
-VERSION = "4.63.dev0"
+VERSION = "4.63"
 
 # standard object name for the Daemon object
 DAEMON_NAME = "Pyro.Daemon"
