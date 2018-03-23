89362abf6dcb7def090453812d4bc2186de3d9d3
Irmen de Jong <irmen@razorvine.net>
2018-03-23 21:58:37 +0100

    doc theme updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,16 +95,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+#import guzzle_sphinx_theme
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
     "rightsidebar": True,
-    "bodyfont": "Tahoma,Helvetica,\"Helvetica Neue\",Arial,sans-serif",
-    "linkcolor": "#3070a0",
-    "visitedlinkcolor": "#3070a0",
+#    "bodyfont": "Tahoma,Helvetica,\"Helvetica Neue\",Arial,sans-serif",
+#    "linkcolor": "#3070a0",
+#    "visitedlinkcolor": "#3070a0",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
